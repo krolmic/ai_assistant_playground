904c7bc3dc239c8d9dbf41186f07a72b747451ef
Michal Krol <hello@krolmic.dev>
2025-07-28 15:09:02 +0800

    chore(app): add basic fimber log tree in bootstrap

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -5,20 +5,49 @@ import 'package:ai_assistant_1/firebase_options.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fimber/flutter_fimber.dart';
 
-class AppBlocObserver extends BlocObserver {
-  const AppBlocObserver();
+class LoggingBlocObserver extends BlocObserver {
+  const LoggingBlocObserver();
+
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    Fimber.v('Creating instance of ${bloc.runtimeType}');
+
+    super.onCreate(bloc);
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    Fimber.v(event.toString());
+
+    super.onEvent(bloc, event);
+  }
 
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    Fimber.v('${change.nextState}');
+
     super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    Fimber.e(
+      'Error in ${bloc.runtimeType}',
+      ex: error,
+      stacktrace: stackTrace,
+    );
+
     super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    Fimber.v('Closing instance of ${bloc.runtimeType}');
+
+    super.onClose(bloc);
   }
 }
 
@@ -27,7 +56,12 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  Bloc.observer = const AppBlocObserver();
+  final logTree = DebugTree(
+    logLevels: DebugTree.defaultLevels.toList()..add('V'),
+    useColors: true,
+  );
+  Fimber.plantTree(logTree);
+  Bloc.observer = const LoggingBlocObserver();
 
   // Add cross-flavor configuration here
   WidgetsFlutterBinding.ensureInitialized();
